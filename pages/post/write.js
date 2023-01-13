7788bcfe7629585e099c680cbb662771b34e14a4
성9 <seong9@seong9ui-MacBookAir.local>
2023-01-13 16:29:57 +0900

    feat: blog deploy

---
+++ pages/post/write.js
@@ -0,0 +1,71 @@
+import Link from "next/link";
+import { useRef, useState } from "react";
+import Layout from "../../components/layout";
+
+export default function Write() {
+  const idRef = useRef();
+  const titleRef = useRef();
+  const contentRef = useRef();
+
+  const [showLink, setShowLink] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const id = idRef.current.value;
+    const title = titleRef.current.value;
+    const content = contentRef.current.value;
+
+    if (id && title && content) {
+      fetch("/api/post/write", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          id,
+          title,
+          content,
+        }),
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("fetch error");
+        })
+        .then((data) => {
+          setShowLink(true);
+          alert(data.message);
+        })
+        .catch((error) => alert(`request error: ${error}`));
+    }
+  };
+
+  return (
+    <Layout>
+      <h1>Write a post</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="id" placeholder="id" required ref={idRef} />
+        <br />
+        <input
+          type="text"
+          name="title"
+          placeholder="title"
+          required
+          ref={titleRef}
+        />
+        <br />
+        <textarea
+          type="text"
+          name="content"
+          placeholder="content"
+          required
+          ref={contentRef}
+        />
+        <br />
+        <input type="submit" value="Create" />
+      </form>
+      {showLink && (
+        <Link href={`/posts/${idRef.current.value}`}>Created Post</Link>
+      )}
+    </Layout>
+  );
+}
